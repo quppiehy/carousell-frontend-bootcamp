e5438e3efa9942d3d0b5cd328245e06e652fca69
quppiehy <sunhuiyi@gmail.com>
2023-08-04 00:30:55 +0800

    added auth0 api

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -3,21 +3,75 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { useAuth0 } from "@auth0/auth0-react";
 
-import { BACKEND_URL } from "../constants.js";
+import { BACKEND_URL } from "../constants";
 
 const Listing = () => {
   const [listingId, setListingId] = useState();
   const [listing, setListing] = useState({});
+  const [currUser, setCurrUser] = useState({});
+  const [accessToken, setAccessToken] = useState("");
+  const {
+    user,
+    isAuthenticated,
+    isLoading,
+    logout,
+    loginWithRedirect,
+    getAccessTokenSilently,
+  } = useAuth0();
 
   useEffect(() => {
-    // If there is a listingId, retrieve the listing data
-    if (listingId) {
-      axios.get(`${BACKEND_URL}/listings/${listingId}`).then((response) => {
-        setListing(response.data);
-      });
+    const checkLogin = async () => {
+      const localAccess = JSON.parse(localStorage.getItem("Token"));
+
+      if (localAccess) {
+        setAccessToken(accessToken);
+      } else if (isAuthenticated) {
+        const accessToken = await getAccessTokenSilently({
+          authorizationParams: {
+            audience: process.env.REACT_APP_AUDIENCE,
+            scope: process.env.REACT_APP_SCOPE,
+          },
+        });
+        console.log("This is access token ", accessToken);
+        setAccessToken(accessToken);
+      }
+
+      if (isAuthenticated && accessToken !== null) {
+        //login
+        const userInfo = await axios.post(
+          `${BACKEND_URL}/listings/login`,
+          user
+        );
+        console.log(userInfo.data.checkedUser);
+        if (userInfo != null) {
+          setCurrUser(userInfo.data.checkedUser);
+        }
+      }
+    };
+    checkLogin();
+    console.log(user);
+  }, [user, isAuthenticated]);
+
+  useEffect(() => {
+    if (accessToken !== null) {
+      localStorage.setItem("Token", JSON.stringify(accessToken));
     }
-    // Only run this effect on change to listingId
+  }, [accessToken]);
+
+  useEffect(() => {
+    const getListing = async () => {
+      // If there is a listingId, retrieve the listing data
+      if (listingId && accessToken !== null) {
+        axios.get(`${BACKEND_URL}/listings/${listingId}`).then((response) => {
+          setListing(response.data);
+        });
+      }
+    };
+
+    getListing();
+    // Only r;un this effect on change to listingId
   }, [listingId]);
 
   // Update listing ID in state if needed to trigger data retrieval
@@ -36,10 +90,25 @@ const Listing = () => {
     }
   }
 
-  const handleClick = () => {
-    axios.put(`${BACKEND_URL}/listings/${listingId}`).then((response) => {
-      setListing(response.data);
-    });
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  const handleClick = async () => {
+    if (isAuthenticated && accessToken !== null) {
+      const output = await axios
+        .put(`${BACKEND_URL}/listings/${listingId}`, currUser, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then((response) => {
+          setListing(response.data);
+        });
+      console.log(output);
+    } else {
+      loginWithRedirect();
+    }
   };
 
   return (
@@ -47,9 +116,32 @@ const Listing = () => {
       <Link to="/">Home</Link>
       <Card bg="dark">
         <Card.Body>
+          {isAuthenticated ? (
+            <div>
+              <button
+                onClick={() => {
+                  localStorage.removeItem("Token");
+                  logout({
+                    logoutParams: { returnTo: window.location.origin },
+                  });
+                }}
+              >
+                Log Out
+              </button>
+              <br />
+              {/* <img src={user.picture} alt={user.name} /> */}
+              <h2>Hello {user.name}!</h2>
+              <p>Your registered email is {user.email}.</p>
+            </div>
+          ) : (
+            <div>
+              <h4>Please login if you wish to buy an item.</h4>
+              <button onClick={() => loginWithRedirect()}>Log In</button>
+            </div>
+          )}
           {listingDetails}
           <Button onClick={handleClick} disabled={listing.BuyerId}>
-            Buy
+            Buy this item
           </Button>
         </Card.Body>
       </Card>
--- src/components/NewListingForm.js
+++ src/components/NewListingForm.js
@@ -1,8 +1,9 @@
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import { BACKEND_URL } from "../constants";
 
@@ -13,7 +14,58 @@ const NewListingForm = () => {
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
   const [shippingDetails, setShippingDetails] = useState("");
+  const [currUser, setCurrUser] = useState({});
+  const [accessToken, setAccessToken] = useState([]);
+
+  // const [user, setUser] = useState("");
   const navigate = useNavigate();
+  const {
+    user,
+    isAuthenticated,
+    isLoading,
+    logout,
+    loginWithRedirect,
+    getAccessTokenSilently,
+  } = useAuth0();
+  // const [userMetadata, setUserMetadata] = useState(null);
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      const accessToken = await getAccessTokenSilently({
+        authorizationParams: {
+          audience: process.env.REACT_APP_AUDIENCE,
+          scope: process.env.REACT_APP_SCOPE,
+        },
+      });
+      setAccessToken(accessToken);
+      // }
+      console.log("Access Token : ", accessToken);
+      console.log("User: ", user);
+      if (
+        isAuthenticated &&
+        accessToken !== null &&
+        typeof user.email !== "undefined"
+      ) {
+        //login
+        const userInfo = await axios.post(
+          `${BACKEND_URL}/listings/login`,
+          user
+        );
+        console.log(userInfo.data.checkedUser);
+        if (userInfo != null) {
+          setCurrUser(userInfo.data.checkedUser);
+        }
+      }
+    };
+    checkLogin();
+    console.log(user);
+  }, [user, isAuthenticated]);
+
+  useEffect(() => {
+    if (accessToken !== null) {
+      localStorage.setItem("Token", JSON.stringify(accessToken));
+    }
+  }, [accessToken]);
 
   const handleChange = (event) => {
     switch (event.target.name) {
@@ -39,36 +91,82 @@ const NewListingForm = () => {
     }
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     // Prevent default form redirect on submission
+    console.log(currUser);
     event.preventDefault();
 
-    // Send request to create new listing in backend
-    axios
-      .post(`${BACKEND_URL}/listings`, {
+    //   {
+    //   // TODO: Replace with your own app's audience. Should be same as API identifier in above steps.
+    //   audience: "https://carousell/api",
+    //   scope: "read:current_user",
+    // }
+
+    if (isAuthenticated && accessToken !== null) {
+      const objToSend = {
         title,
         category,
         condition,
         price,
         description,
         shippingDetails,
-      })
-      .then((res) => {
-        // Clear form state
-        setTitle("");
-        setCategory("");
-        setCondition("");
-        setPrice(0);
-        setDescription("");
-        setShippingDetails("");
+        currUser,
+      };
 
-        // Navigate to listing-specific page after submitting form
-        navigate(`/listings/${res.data.id}`);
-      });
+      // Send request to create new listing in backend
+      const output = await axios
+        .post(`${BACKEND_URL}/listings`, objToSend, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then((res) => {
+          // Clear form state
+          console.log(output);
+          setTitle("");
+          setCategory("");
+          setCondition("");
+          setPrice(0);
+          setDescription("");
+          setShippingDetails("");
+          // Navigate to listing-specific page after submitting form
+          navigate(`/listings/${res.data.id}`);
+        });
+    } else {
+      loginWithRedirect();
+    }
   };
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
+      {isAuthenticated ? (
+        <div>
+          <button
+            onClick={() => {
+              localStorage.removeItem("Token");
+              logout({
+                logoutParams: { returnTo: window.location.origin },
+              });
+            }}
+          >
+            Log Out
+          </button>
+          <br />
+          {/* <img src={user.picture} alt={user.name} /> */}
+          <h2>Hello {user.name}!</h2>
+          <p>Your registered email is {user.email}.</p>
+        </div>
+      ) : (
+        <div>
+          <h4>Please login to list a new listing.</h4>
+          <button onClick={() => loginWithRedirect()}>Log In</button>
+        </div>
+      )}
+      <br />
       <Form.Group>
         <Form.Label>Title</Form.Label>
         <Form.Control
